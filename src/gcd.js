059b36503fc002bde074356dbc8009e6b43c48da
Nikita <nik_voronkov_1996@mail.ru>
2024-04-27 09:59:31 +0700

    changed by eslint

--- src/gcd.js
+++ src/gcd.js
@@ -1,37 +1,40 @@
 import readlineSync from 'readline-sync';
 
 function gcd(a, b) {
-	while (b !== 0) {
-		let temp = b;
-		b = a % b;
-		a = temp;
-	}
-	return a;
+  let aCopy = a;
+  let bCopy = b;
+
+  while (bCopy !== 0) {
+    const temp = bCopy;
+    bCopy = aCopy % bCopy;
+    aCopy = temp;
+  }
+  return aCopy;
 }
 
 export default function theLargestCommonDivisor(name) {
-	let correctAnswersCount = 0;
-	console.log('Find the greatest common divisor of given numbers.');
+  let correctAnswersCount = 0;
+  console.log('Find the greatest common divisor of given numbers.');
 
-	while (correctAnswersCount < 3) {
-		const randomNumber1 = Math.floor(Math.random() * 100) + 1;
-		const randomNumber2 = Math.floor(Math.random() * 100) + 1;
+  while (correctAnswersCount < 3) {
+    const randomNumber1 = Math.floor(Math.random() * 100) + 1;
+    const randomNumber2 = Math.floor(Math.random() * 100) + 1;
 
-		const correctAnswer = gcd(randomNumber1, randomNumber2);
+    const correctAnswer = gcd(randomNumber1, randomNumber2);
 
-		console.log(`Question: ${randomNumber1} ${randomNumber2}`);
-		let userAnswer = readlineSync.question('Your answer: ').toLowerCase();
+    console.log(`Question: ${randomNumber1} ${randomNumber2}`);
+    const userAnswer = readlineSync.question('Your answer: ').toLowerCase();
 
-		if (correctAnswer === Number(userAnswer)) {
-			correctAnswersCount += 1;
-			console.log('Correct!');
-		} else {
-			console.log(
-				`${userAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}.`
-			);
-			console.log(`Let's try again, ${name}!`);
-			return;
-		}
-	}
-	console.log(`Congratulations, ${name}!`);
+    if (correctAnswer === Number(userAnswer)) {
+      correctAnswersCount += 1;
+      console.log('Correct!');
+    } else {
+      console.log(
+        `${userAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}.`,
+      );
+      console.log(`Let's try again, ${name}!`);
+      return;
+    }
+  }
+  console.log(`Congratulations, ${name}!`);
 }
--- src/isPrimeNumber.js
+++ src/isPrimeNumber.js
@@ -1,45 +1,45 @@
 import readlineSync from 'readline-sync';
 
 function generatePrimeNumber() {
-	console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
-	let randomNumber1 = Math.floor(Math.random() * 1000) + 2;
-	console.log(`Question: ${randomNumber1}`);
-	for (let i = 2; i <= Math.sqrt(randomNumber1); i += 1) {
-		if (randomNumber1 % i === 0) {
-			return false;
-		}
-	}
-	return true;
+  console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+  const randomNumber1 = Math.floor(Math.random() * 1000) + 2;
+  console.log(`Question: ${randomNumber1}`);
+  for (let i = 2; i <= Math.sqrt(randomNumber1); i += 1) {
+    if (randomNumber1 % i === 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 export default function isPrimeNumber(name) {
-	let correctAnswersCount = 0;
-	while (correctAnswersCount < 3) {
-		let isPrime = generatePrimeNumber();
-		let correctAnswer = isPrime ? 'yes' : 'no';
-		let userAnswer = readlineSync.question('Your answer: ').toLowerCase();
-		if (userAnswer === correctAnswer) {
-			console.log('Correct!');
-			correctAnswersCount += 1;
-		} else {
-			console.log(
-				`${userAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}.`
-			);
-			console.log(`Let's try again, ${name}!`);
-			return;
-		}
-	}
-	console.log(`Congratulations, ${name}!`);
+  let correctAnswersCount = 0;
+  while (correctAnswersCount < 3) {
+    const isPrime = generatePrimeNumber();
+    const correctAnswer = isPrime ? 'yes' : 'no';
+    const userAnswer = readlineSync.question('Your answer: ').toLowerCase();
+    if (userAnswer === correctAnswer) {
+      console.log('Correct!');
+      correctAnswersCount += 1;
+    } else {
+      console.log(
+        `${userAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}.`,
+      );
+      console.log(`Let's try again, ${name}!`);
+      return;
+    }
+  }
+  console.log(`Congratulations, ${name}!`);
 }
 
-function isPrimeCheck(number) {
-	if (number < 2) {
-		return false;
-	}
-	for (let i = 2; i <= Math.sqrt(number); i += 1) {
-		if (number % i === 0) {
-			return false;
-		}
-	}
-	return true;
-}
+/* function isPrimeCheck(number) {
+  if (number < 2) {
+    return false;
+  }
+  for (let i = 2; i <= Math.sqrt(number); i += 1) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+  return true;
+} */
